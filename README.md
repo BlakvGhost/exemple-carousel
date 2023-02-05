61bd5987cc7b970eb5e97f4ae6423da6c1004677
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-02-05 03:04:57 +0100

    Carousel::first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Carousel
+
+Bienvenue sur le projet Carousel ! Ce projet a pour but de vous présenter un carrousel simple et fonctionnel réalisé en HTML, CSS et JavaScript. Il utilise les propriétés CSS `flexbox` et `translateX` pour faire défiler les images horizontalement, et les boutons "Précédent" et "Suivant" pour contrôler manuellement le défilement. Il y a également une fonctionnalité d'auto-défilement avec des points de navigation pour passer d'une image à l'autre.
+
+## Prérequis
+
+Pour utiliser ce projet, vous avez besoin des connaissances de base en HTML, CSS et JavaScript.
+
+## Installation
+
+Pour utiliser ce projet, veuillez cloner le repository Github sur votre ordinateur local et ouvrir le fichier `index.html` avec un navigateur web.
+
+## Structure du projet
+
+Le projet est composé des fichiers suivants:
+
+`index.html` : Le fichier HTML principal qui contient le code HTML du carrousel
+`style.css` : Le fichier CSS qui contient la mise en forme du carrousel
+`app.js` : Le fichier JavaScript qui contient le code JavaScript du carrousel
+
+## Fonctionnalités
+
+- Affichage des slides de manière continue et infinie
+- Possibilité de naviguer entre les slides avec les boutons Précédent et Suivant
+- Possibilité de naviguer entre les slides en cliquant sur les points (dots)
+- Possibilité de naviguer entre les slides sur les `smartphone en glissant sur les images`
+- Mise en pause du carrousel lorsque la souris se trouve sur un slide et reprise de la rotation lorsque la souris quitte le slide
+- Temps de rotation réglable en modifiant la variable `slideTimeout` dans le fichier JavaScript
+
+## Utilisation
+
+Clonez ce repository sur votre ordinateur.
+Ouvrez le fichier `index.html` avec un navigateur web.
+Vous devriez voir le carousel en action avec un bouton de navigation vers la gauche et un bouton de navigation vers la droite.
+Il y a également des points de navigation en bas du carousel pour naviguer entre les diapositives.
+Le carousel défile automatiquement toutes les 5 secondes. Vous pouvez mettre en pause le défilement en passant la souris sur le carousel.
+
+## Personnalisation
+
+Vous pouvez personnaliser le carousel en modifiant les fichiers CSS et JavaScript.
+
+Modifiez le fichier `style.css` pour changer la mise en page et les couleurs du carousel.
+Modifiez le fichier `app.js` pour changer la durée de l'intervalle de défilement, la logique de navigation, etc.
+
+## Conclusion
+
+J'espère que ce projet vous a plu et vous a été utile. Si vous avez des suggestions ou des commentaires, n'hésitez pas à me les faire parvenir via Github. Merci d'avoir utilisé le projet Carousel !
+
+## Licence
+
+Ce projet est sous licence MIT.
---
+++ app.js
@@ -0,0 +1,97 @@
+// Ceci est une fonction auto - exécutable.Les fonctions auto - exécutables
+// sont des fonctions qui s'exécutent immédiatement après leur déclaration,
+// sans avoir besoin d'être appelées.Les accolades immédiatement après la 
+// déclaration de la fonction et les parenthèses à la fin de la déclaration 
+// définissent la fonction et permettent de l'exécuter immédiatement.
+(function () {
+    // Utilisation de la directive "use strict" pour activer le mode strict en JavaScript
+    // Cela implique une meilleure gestion des erreurs et une syntaxe plus stricte pour le code
+    "use stict"
+
+    // Déclare la constante pour la durée de chaque slide
+    const slideTimeout = 5000;
+
+    // Récupère les boutons de navigation
+    const prev = document.querySelector('#prev');
+    const next = document.querySelector('#next');
+
+    // Récupère tous les éléments de type "slide"
+    const $slides = document.querySelectorAll('.slide');
+
+    // Initialisation de la variable pour les "dots"
+    let $dots;
+
+    // Initialisation de la variable pour l'intervalle d'affichage des slides
+    let intervalId;
+
+    // Initialisation du slide courant à 1
+    let currentSlide = 1;
+
+    // Fonction pour afficher un slide spécifique en utilisant un index
+    function slideTo(index) {
+        // Vérifie si l'index est valide (compris entre 0 et le nombre de slides - 1)
+        currentSlide = index >= $slides.length || index < 1 ? 0 : index;
+
+        // Boucle sur tous les éléments de type "slide" pour les déplacer
+        $slides.forEach($elt => $elt.style.transform = `translateX(-${currentSlide * 100}%)`);
+
+        // Boucle sur tous les "dots" pour mettre à jour la couleur par la classe "active" ou "inactive"
+        $dots.forEach(($elt, key) => $elt.classList = `dot ${key === currentSlide? 'active': 'inactive'}`);
+    }
+
+    // Fonction pour afficher le prochain slide
+    function showSlide() {
+        slideTo(currentSlide);
+        currentSlide++;
+    }
+
+    // Boucle pour créer les "dots" en fonction du nombre de slides
+    for (let i = 1; i <= $slides.length; i++) {
+        let dotClass = i == currentSlide ? 'active' : 'inactive';
+        let $dot = `<span data-slidId="${i}" class="dot ${dotClass}"></span>`;
+        document.querySelector('.carousel-dots').innerHTML += $dot;
+    }
+
+    // Récupère tous les "dots"
+    $dots = document.querySelectorAll('.dot');
+
+    // Boucle pour ajouter des écouteurs d'événement "click" sur chaque "dot"
+    $dots.forEach(($elt, key) => $elt.addEventListener('click', () => slideTo(key)));
+
+    // Ajout d'un écouteur d'événement "click" sur le bouton "prev" pour afficher le slide précédent
+    prev.addEventListener('click', () => slideTo(--currentSlide))
+
+    // Ajout d'un écouteur d'événement "click" sur le bouton "next" pour afficher le slide suivant
+    next.addEventListener('click', () => slideTo(++currentSlide))
+
+    // Initialisation de l'intervalle pour afficher les slides
+    intervalId = setInterval(showSlide, slideTimeout)
+    // Boucle sur tous les éléments de type "slide" pour ajouter des écouteurs d'événement pour les interactions avec la souris et le toucher
+    $slides.forEach($elt => {
+        let startX;
+        let endX;
+        // Efface l'intervalle d'affichage des slides lorsque la souris passe sur un slide
+        $elt.addEventListener('mouseover', () => {
+            clearInterval(intervalId);
+        }, false)
+        // Réinitialise l'intervalle d'affichage des slides lorsque la souris sort d'un slide
+        $elt.addEventListener('mouseout', () => {
+            intervalId = setInterval(showSlide, slideTimeout);
+        }, false);
+        // Enregistre la position initiale du toucher lorsque l'utilisateur touche un slide
+        $elt.addEventListener('touchstart', (event) => {
+            startX = event.touches[0].clientX;
+        });
+        // Enregistre la position finale du toucher lorsque l'utilisateur relâche son doigt
+        $elt.addEventListener('touchend', (event) => {
+            endX = event.changedTouches[0].clientX;
+            // Si la position initiale est plus grande que la position finale, affiche le prochain slide
+            if (startX > endX) {
+                slideTo(currentSlide + 1);
+                // Si la position initiale est plus petite que la position finale, affiche le slide précédent
+            } else if (startX < endX) {
+                slideTo(currentSlide - 1);
+            }
+        });
+    })
+})()
